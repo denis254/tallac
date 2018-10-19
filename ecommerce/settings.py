15894fc65532a0e040ac23cc9f16b0fa29c4fe96
denis kiprono <deniskiprono4@gmail.com>
2018-10-19 14:40:34 +0300

    First

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages',
 ]
 
 MIDDLEWARE = [
@@ -80,8 +79,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tallaglobal',
+        'USER': 'denis',
+        'PASSWORD': '?254Denis452??',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
